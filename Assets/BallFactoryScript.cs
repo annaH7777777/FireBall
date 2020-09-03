43c50b8f9ccbc3494a21097fcf4f6b0af0964e2a
Anna <49167607+annaH7777777@users.noreply.github.com>
2020-09-03 23:22:55 +0300

    Start of the project

---
+++ Assets/BallFactoryScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFactoryScript : MonoBehaviour
+{
+    public Transform ballFactoryPoint;
+    public GameObject ballPrefab;
+    public float ballForce;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Update is called once per frame
+    void Start()
+    {
+
+        InvokeRepeating("Fall", startDelay, spawnInterval);
+       
+    }
+    void Update()
+    {
+        
+    }
+    void Fall()
+    {
+        GameObject ball = Instantiate(ballPrefab, ballFactoryPoint.position, ballFactoryPoint.rotation);
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.AddForce(ballFactoryPoint.up * ballForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/BulletScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            Debug.Log("Collision");
+        }
+        
+        //else GameOver();
+    }
+}
